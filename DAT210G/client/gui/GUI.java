0f54359adb3b92275f344851b521e0440ad8bf98
javaronnie <ronnie.bjorkelund@gmail.com>
2014-02-07 01:55:20 +0100

    Andre utkast til GUI
    Bare prototyp for min egen testing foreløpig. Veldig rotete kode, lite
    oop osv.

--- DAT210G/client/gui/GUI.java
+++ DAT210G/client/gui/GUI.java
@@ -1,93 +1,85 @@
 package gui;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.event.ActionListener;
+import java.awt.Dimension;
+import java.util.ArrayList;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.border.LineBorder;
+import javax.swing.JTextField;
 
 public class GUI extends JFrame {
-	
+
+	/**
+	 * 
+	 */
 	private static final long serialVersionUID = 1L;
-	
-	private static final int WIDTH = 500;
-	private static final int HEIGHT = 500;
-	
-	private int imgGridColumns = 3;
-	private int imgGridRows = 6;
-	
-	private JButton searchBtn;
-	private JButton importBtn;
-	private JPanel navPanel;
-	private ImagePanel imgPanel;
-	
-	
-	
+	private JPanel singleImageSidebar;
+	private JPanel multiImageSidebar;
+	private JPanel imageViewPanel;
+	private JScrollPane scroll;
+
 	public GUI() {
-		
-		makeGUI();
-		
-	}
-	
-	private void makeGUI() {
-		
-		setTitle("Prosjekt Client GUI");
-		setSize(WIDTH, HEIGHT);
-		
-		
-		
-		/*
-		 * navPanel for navigation
-		 * 
-		 * Buttons etc.
-		 * 
-		 */
-		navPanel = new JPanel();
-		navPanel.setLayout(new BoxLayout(navPanel, BoxLayout.PAGE_AXIS));;	
-		navPanel.setBorder(new LineBorder(Color.BLUE));
-		add(navPanel, BorderLayout.WEST);
-		
-		
-		/*
-		 * imgPanel
-		 * 
-		 * Area for images
-		 * 
-		 */				
-		imgPanel = new ImagePanel(imgGridRows, imgGridColumns);		
+		this.setSize(800, 600);
+		this.setTitle("GUI Prototyp");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-		JScrollPane scroll = new JScrollPane(imgPanel);
+		this.setLayout(new BorderLayout());
+
+		singleImageSidebar = new JPanel();
+		multiImageSidebar = new JPanel();
+		imageViewPanel = new JPanel();
+
+		initImageViewPanel();
+		initSingleImageSidebar();
+		initMultiImageSidebar();
 		
-		add(scroll, BorderLayout.CENTER);
+		scroll = new JScrollPane(imageViewPanel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		
-		/*
-		 * Buttons
-		 * 
-		 * 
-		 */
-		searchBtn = new JButton("Search");
-		navPanel.add(searchBtn);
-		importBtn = new JButton("Import");
-		navPanel.add(importBtn);
 		
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.add(multiImageSidebar, BorderLayout.WEST);
+		this.add(scroll, BorderLayout.CENTER);
+
+		this.setVisible(true);
+
 	}
 
-	
-	public void addSearchBtnListener(ActionListener listenForSearchBtn) {
-		searchBtn.addActionListener(listenForSearchBtn);
+	private void initImageViewPanel() {
+		
+		imageViewPanel.setBounds(0, 0, 200, 2000);  
+		imageViewPanel.setPreferredSize(new Dimension(200, 2000));  
 	}
 	
-	public void addImportBtnListener(ActionListener listenForImportBtn) {
-		importBtn.addActionListener(listenForImportBtn);
+	private void initSingleImageSidebar() {
+		singleImageSidebar.setLayout(new BoxLayout(singleImageSidebar, BoxLayout.PAGE_AXIS));
+		
+		singleImageSidebar.add(new JButton("Next"));
+		singleImageSidebar.add(new JButton("Previous"));
+		singleImageSidebar.add(new JButton("Rotate clockwise"));
+		singleImageSidebar.add(new JButton("Rotate anticlockwise"));
+		singleImageSidebar.add(new JButton("Edit Metadata"));
+	}
+	
+	private void initMultiImageSidebar() {
+		multiImageSidebar.setLayout(new BoxLayout(multiImageSidebar, BoxLayout.PAGE_AXIS));
+
+		multiImageSidebar.add(new JTextField("Rating"));
+		multiImageSidebar.add(new JTextField("Name"));
+		multiImageSidebar.add(new JTextField("Tags"));
+		multiImageSidebar.add(new JTextField("Date"));
+		multiImageSidebar.add(new JTextField("Etc"));
+		multiImageSidebar.add(new JButton("Search"));
+		multiImageSidebar.add(new JButton("Import"));
 	}
 
-	
+	public void drawImagesFromList(ArrayList<JPanel> imageList) {
+		for (JPanel imagePanel : imageList) {			
+			imageViewPanel.add(imagePanel);			
+		}
+		imageViewPanel.repaint();
+	}
 	
 }
