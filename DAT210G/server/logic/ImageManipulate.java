d20b6737005c9d50eea1f2ed614e42888e601628
Ronny Wathne <spacew@gmail.com>
2014-02-14 08:16:57 +0000

    Create ImageManipulate.java

---
+++ DAT210G/server/logic/ImageManipulate.java
@@ -0,0 +1,35 @@
+package logic;
+
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+
+public class ImageManipulate {
+
+	
+	public BufferedImage RotateImage90Degrees(BufferedImage imageFile) {
+		int H =imageFile.getHeight();
+		int W =imageFile.getWidth();
+		
+		BufferedImage orginal = imageFile;
+		
+		BufferedImage Rotert = new BufferedImage(H, W, BufferedImage.TYPE_INT_RGB);
+		
+		double theta = Math.PI / 2;
+	
+		AffineTransform xform = AffineTransform.getRotateInstance(theta, W / 2, W / 2);
+		Graphics2D g = (Graphics2D) Rotert.createGraphics();
+		g.drawImage(orginal, xform, null);
+		g.dispose();
+	
+		return Rotert;
+	
+	}
+	
+	public static void main(String[] args) {
+		// Test
+		
+		
+	}
+
+}
